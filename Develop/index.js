15edd9fed6751ace386629e5615ca27b9513883e
Thomas P <132946099+Tomjp424@users.noreply.github.com>
2024-08-28 19:12:37 -0400

    testing new method

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,98 @@
 // TODO: Include packages needed for this application
+import fs from "fs";
+import inquirer from "inquirer";
+import generateMarkdown from "./utils/generateMarkdown.js";
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "Please enter your full name",
+    "Please enter your GitHub username",
+    "Please provide a link to your GitHub profile",
+    "Please enter your email",
+    "What is the title of your project?",
+    "Provide a short description of your project's function.",
+    "Please provide a step-by-step guide on installing your project.",
+    "Please provide instructions on how to use your project, including images if necessary.",
+    "Please specify guidelines for others to contribute to your project",
+    "Please provide instructions for proper testing of your application",
+    "What type of license does your project use?",
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+const licenseMap = {
+    "MIT": "MIT",
+    "Creative Commons Zero": "CC0",
+    "Apache 2.0": "Apache",
+    "Mozilla Public License 2.0": "MPL",
+    "No License": "",
+}
+
+inquirer
+    .prompt([
+        {
+            name: "fullName",
+            type: "input",
+            message: questions[0],  // "Please enter your full name"
+        },
+        {
+            name: "gitUsername",
+            type: "input",
+            message: questions[1],  // "Please enter your GitHub username"
+        },
+        {
+            name: "gitLink",
+            type: "input",
+            message: questions[2],  // "Please provide a link to your GitHub profile"
+        },
+        {
+            name: "email",
+            type: "input",
+            message: questions[3],  // "Please enter your email"
+        },
+        {
+            name: "title",
+            type: "input",
+            message: questions[4],  // "What is the title of your project?"
+        },
+        {
+            name: "description",
+            type: "input",
+            message: questions[5],  // "Provide a short description of your project's function."
+        },
+        {
+            name: "installGuide",
+            type: "input",
+            message: questions[6],  // "Please provide a step-by-step guide on installing your project."
+        },
+        {
+            name: "usageGuide",
+            type: "input",
+            message: questions[7],  // "Please provide instructions on how to use your project, including images if necessary."
+        },
+        {
+            name: "contributionGuide",
+            type: "input",
+            message: questions[8],  // "Please specify guidelines for others to contribute to your project"
+        },
+        {
+            name: "testGuide",
+            type: "input",
+            message: questions[9],  // "Please provide instructions for proper testing of your application"
+        },
+        {
+            name: "license",
+            type: "list",
+            message: questions[10],  // "What type of license does your project use?"
+            choices: ["MIT", "Creative Commons Zero", "Apache 2.0", "Mozilla Public License 2.0", "No License"]
+        },
+    ]) .then ((responses) => {
+        const licenseChoice = licenseMap[responses.license];
+    })
+
+function writeToFile(fileName, data) {
+    fs.writeFile("README.md", )
+}
 
 // TODO: Create a function to initialize app
 function init() {}
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,38 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const licenseBadges = {
+    MIT: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]",
+    CC0: "[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)]",
+    Apache: "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]",
+    MPL: "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)",
+  }
+  return license ? licenseBadges.license : "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const licenseLinks = {
+    MIT: "(https://opensource.org/licenses/MIT)",
+    CC0: "(http://creativecommons.org/publicdomain/zero/1.0/)",
+    Apache: "(https://opensource.org/licenses/Apache-2.0)",
+    MPL: "(https://opensource.org/licenses/MPL-2.0)",
+  }
+  return license ? licenseLinks.license : "";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(responses) {
+  if (responses.license !== "No License") {
+    return `## License
+
+Copyright (c) ${responses.fullName}.
+
+Licensed under the [${responses.license}](${renderLicenseLink(licenseChoice)}) license.`
+  } else return ""
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
